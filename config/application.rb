fc9bf9435ab07a3b0e9016f4880931598c77fb0d
Saumya Vardhan Kanoria <svk@breakingwavesoftware.com>
2014-10-01 14:47:54 +0530

    configured ActiveJob to use DelayedJob as the task runner

--- config/application.rb
+++ config/application.rb
@@ -1,12 +1,13 @@
 require File.expand_path('../boot', __FILE__)
 
 # Pick the frameworks you want:
-require "active_model/railtie"
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "sprockets/railtie"
+require 'active_model/railtie'
+require 'active_record/railtie'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_view/railtie'
+require 'sprockets/railtie'
+require 'active_job/railtie' # MY NOTE: Added this to enable ActiveJob
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems you've limited to
@@ -60,5 +61,8 @@ module RailsAppStarterKit
 
       g.fixture_replacement :factory_girl, dir: 'spec/factories'
     10 end
+
+    # MY NOTE: Tell ActiveJob to use DelayedJob as the background task queue
+    config.active_job.queue_adapter = :delayed_job
   end
 end
---
+++ config/initializers/delayed_job.rb
@@ -0,0 +1,10 @@
+# Copied and edited from
+# https://github.com/collectiveidea/delayed_job#gory-details.
+Delayed::Worker.destroy_failed_jobs = false
+Delayed::Worker.sleep_delay = 60
+Delayed::Worker.max_attempts = 3
+Delayed::Worker.max_run_time = 1.day # TODO Job max run time too long?
+Delayed::Worker.read_ahead = 10
+Delayed::Worker.default_queue_name = 'default'
+Delayed::Worker.delay_jobs = !Rails.env.test?
+Delayed::Worker.raise_signal_exceptions = :term
