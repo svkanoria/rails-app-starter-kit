61cf3830083e1a19b239bf4c4fd13e57ee0f2030
Saumya Vardhan Kanoria <svk@breakingwavesoftware.com>
2014-11-10 12:15:31 +0530

    better integration between client and server views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,5 @@
 # The application 'entry point' controller.
 class HomeController < ApplicationController
   def index
-    # Currently signed in user details (if any) to be passed on to the
-    # client-side JS code.
-    if current_user
-      @current_user_json = {
-          id: current_user.id,
-          email: current_user.email,
-          roles: current_user.roles.pluck(:name)
-      }.to_json
-    end
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # Currently signed in user details (if any) to be passed on to the
+  # client-side JS code.
+  def current_user_json
+    if current_user
+      { id: current_user.id,
+        email: current_user.email,
+        roles: current_user.roles.pluck(:name) }.to_json
+    end
+  end
 end
