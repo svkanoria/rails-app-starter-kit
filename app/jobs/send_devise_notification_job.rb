2e1dc94e171a3080b083ffac3947d16cf3b4fb14
Saumya Vardhan Kanoria <svk@breakingwavesoftware.com>
2015-03-05 15:33:08 +0530

    added MIT license. other small cleanup

--- app/jobs/send_devise_notification_job.rb
+++ app/jobs/send_devise_notification_job.rb
@@ -4,7 +4,7 @@ class SendDeviseNotificationJob < ActiveJob::Base
   # @param user [User] the user
   # @param notification_str [String] the Devise notification type (a symbol)
   #   converted to a string. Symbols are not serializable by ActiveJob
-  # @param *args other arguments
+  # @param args other arguments
   def perform (user, notification_str, *args)
     user.orig_send_devise_notification(notification_str.to_sym, *args)
   end
