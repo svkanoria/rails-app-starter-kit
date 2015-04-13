93d443416ac47db937cb623af435898c308a35c2
Saumya Vardhan Kanoria <svk@breakingwavesoftware.com>
2015-04-13 10:44:40 +0530

    improved documentation for attachment-library directive

--- app/assets/javascripts/shared/angular/modules/attachment_library/directives/attachment_library_directive.js
+++ app/assets/javascripts/shared/angular/modules/attachment_library/directives/attachment_library_directive.js
@@ -7,10 +7,14 @@
  *
  * The uploader options are passed to the uploader directive.
  * We provide one uploader (which we then use by default): The 'fine-uploader'
- * directive. However, you can create you own if you wish. If you do roll your
- * own, you must also modify this directive's template to use your uploader
- * instead:
- * /app/assets/javascripts/templates/shared/directives/attachment_library.html
+ * directive. However, you can create you own if you wish.
+ *
+ * To create your own uploader, refer to fine_uploader.js for guidance.
+ * In particular, take care to do the following:
+ * * Modify this directive's template to use your uploader instead:
+ *   /app/assets/javascripts/templates/shared/directives/attachment_library.html
+ * * Have your uploader call AttachmentLibrarySvc.emitUploadSuccessful() upon
+ *   any successful upload
  */
 angular.module('AttachmentLibraryDirective', ['AttachmentLibrarySvc']).
   directive('attachmentLibrary', [
