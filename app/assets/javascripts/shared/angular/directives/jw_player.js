2ce9ec01b29e1e2f52885b1ab6c981a9728fc8bc
Saumya Vardhan Kanoria <svk@breakingwavesoftware.com>
2015-05-30 11:59:16 +0530

    more work on attachments. added JWPlayer - basics now working

---
+++ app/assets/javascripts/shared/angular/directives/jw_player.js
@@ -0,0 +1,38 @@
+/*
+ * A directive for the JWPlayer video player.
+ * See http://support.jwplayer.com.
+ *
+ * Usage:
+ *   <div jw-player id="some-id"
+ *                  options="Object expr">
+ *   </div>
+ */
+angular.module('JWPlayer', []).
+  directive('jwPlayer', [
+    function () {
+      /**
+       * Sensible defaults likely to always apply.
+       * Can be overridden by any options directly passed to the directive.
+       *
+       * @type {Object}
+       */
+      var DEFAULT_OPTS = {
+        width: '100%',
+        aspectratio: '16:9'
+      };
+
+      return {
+        restrict: 'A',
+
+        scope: {
+          options: '='
+        },
+
+        link: function (scope, element, attrs) {
+          var finalOpts = {};
+          _.extend(finalOpts, DEFAULT_OPTS, scope.options);
+
+          jwplayer(attrs.id).setup(finalOpts);
+        }
+      }
+    }]);
