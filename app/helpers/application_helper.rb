90253c29639963a84d0bd82688ecefe6f3455d6a
Saumya Kanoria <svk@breakingwavesoftware.com>
2016-07-08 11:33:03 +0530

    fixed small ui bugs. small change to README

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,21 +8,4 @@ module ApplicationHelper
         roles: current_user.roles.pluck(:name) }.to_json
     end
   end
-
-  # Mapping of Rails flash keys to Bootstrap alert contexts. Contains only the
-  # keys that do not map unchanged (i.e. keys that have no namesake Bootstrap
-  # alert contexts).
-  FLASH_KEY_MAP = { notice: 'success', alert: 'danger' }
-
-  # Maps Rails flash keys to appropriate Bootstrap alert contexts.
-  #
-  # This is required because the most commonly used Rails flash keys (notice,
-  # alert) have no namesake Bootstrap alert contexts.
-  #
-  # @param flash_key [String, Symbol]
-  #
-  # @return [String]
-  def bootstrap_alert_context (flash_key)
-    FLASH_KEY_MAP[flash_key.to_sym] || flash_key
-  end
 end
